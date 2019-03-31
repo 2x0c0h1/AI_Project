b4993a8065b1116f6dd2ea976aa5ea75dc303891
Xavier <xavieroh05@gmail.com>
2019-03-31 22:07:24 +0800

    Included some documentation

--- image_classifier.py
+++ image_classifier.py
@@ -1,36 +1,31 @@
-from keras import applications
-
+from keras import applications, optimizers
 from keras.preprocessing.image import ImageDataGenerator
-
-from keras import optimizers
 from keras.models import Sequential, Model
 from keras.layers import (Dropout, Flatten, Dense, Conv2D,
                           Activation, MaxPooling2D)
-
 from keras_tqdm import TQDMCallback
 
+#Checking GPU usage
 from keras import backend as K
-if K.tensorflow_backend._get_available_gpus() == []:
-    print("Not using GPU!!!")
+print(K.tensorflow_backend._get_available_gpus())
 
-# dimensions of our images. label = bezos, gates 2 labels
+#Image Dimensions
 img_width, img_height = 128, 128
 
+#Image folder directories
 train_data_dir = 'training_images'
 validation_data_dir = 'test_images'
 
-epochs = 50
-batch_size = 16
-
+#Building Model
 model = Sequential()
 
 model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3)))
-model.add(Activation('relu')) #tanh
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.5))
 
 model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu')) #tanh
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.5))
 
@@ -56,9 +51,10 @@ train_datagen = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=False)
 
-# this is the augmentation configuration we will use for testing:
-# only rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+epochs = 50
+batch_size = 16
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -74,7 +70,7 @@ validation_generator = test_datagen.flow_from_directory(
 
 model.fit_generator(
     train_generator,
-    steps_per_epoch= 3000 // 16, # give me more data
+    steps_per_epoch= 3000 // 16,
     epochs=epochs,
     verbose=0,
     callbacks=[TQDMCallback()],
